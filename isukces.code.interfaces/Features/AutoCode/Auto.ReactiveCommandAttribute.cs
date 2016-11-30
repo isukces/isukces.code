abf01d9e78aa59d10fc6021e3a6d3a1bf016991c
Piotr Stęclik <piotr@steclik.com>
2016-11-30 19:47:45 +0100

    annotacja dla ReactiveUi

---
+++ isukces.code.interfaces/Features/AutoCode/Auto.ReactiveCommandAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace isukces.code.interfaces
+{
+    public partial class Auto
+    {
+        [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+        public class ReactiveCommandAttribute : Attribute
+        {
+            #region Constructors
+
+            public ReactiveCommandAttribute(string name, Type resultType)
+            {
+                Name = name;
+                ResultType = resultType;
+            }
+
+            #endregion
+
+            #region Properties
+
+            public string Name { get; set; }
+            public Type ResultType { get; set; }
+
+            #endregion
+        }
+    }
+}
---
+++ isukces.code.interfaces/Features/AutoCode/Auto.ReactivePropertyAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace isukces.code.interfaces
+{
+    public partial class Auto
+    {
+        [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+        public class ReactivePropertyAttribute : Attribute
+        {
+            #region Constructors
+
+            public ReactivePropertyAttribute(string name, Type propertyType)
+            {
+                Name = name;
+                PropertyType = propertyType;
+            }
+
+            #endregion
+
+            #region Properties
+
+            public string Name { get; set; }
+            public Type PropertyType { get; set; }
+
+            #endregion
+        }
+    }
+}
