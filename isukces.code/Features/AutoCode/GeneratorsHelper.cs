279d321a17bb4de24a44a4f77680240675bc0b0c
Piotr Stęclik <piotr@steclik.com>
2016-12-17 12:42:53 +0100

    better support for byte and DateTime? types

--- isukces.code/Features/AutoCode/GeneratorsHelper.cs
+++ isukces.code/Features/AutoCode/GeneratorsHelper.cs
@@ -14,131 +14,15 @@ namespace isukces.code.AutoCode
     {
         #region Static Methods
 
-        public static HashSet<T> MakeCopy<T>(IEnumerable<T> source, IEnumerable<T> append = null,
-            IEnumerable<T> remove = null)
+        public static string FieldName(string x)
         {
-            var s = new HashSet<T>();
-            if (source != null)
-                foreach (var i in source)
-                    s.Add(i);
-            if (append != null)
-                foreach (var i in append)
-                    s.Add(i);
-            if (remove != null)
-                foreach (var i in remove)
-                    s.Remove(i);
-            return s;
+            return "_" + x.Substring(0, 1).ToLower() + x.Substring(1);
         }
-
-        public static string TypeName(Type type, INamespaceContainer container)
-        {
-            //todo: Generic types
-            if (type == null)
-                return null;
-            if (type.IsArray)
-            {
-                var arrayRank = type.GetArrayRank();
-                var st = TypeName(type.GetElementType(), container);
-                if (arrayRank < 2)
-                    return st + "[]";
-                return string.Format("{0}[{1}]", st, new string(',', arrayRank - 1));
-            }
-            var simple = SimpleTypeName(type);
-            if (!String.IsNullOrEmpty(simple))
-                return simple;
-            if (type.DeclaringType != null)
-                return TypeName(type.DeclaringType, container) + "." + type.Name;
-
-            string fullName;
-            {
-                if (type.IsGenericType)
-                {
-                    var gt = type.GetGenericTypeDefinition();
-                    var args = type.GetGenericArguments();
-                    var args2 = String.Join(",", args.Select(a => TypeName(a, container)));
-                    fullName = gt.FullName.Split('`')[0] + "<" + args2 + ">";
-                }
-                else
-                    fullName = type.FullName;
-            }
-            var typeNamespace = type.Namespace;
-            return !String.IsNullOrEmpty(typeNamespace) &&
-                   (container?.GetNamespaces(true)?.Contains(typeNamespace) ?? false)
-                ? fullName.Substring(typeNamespace.Length + 1)
-                : fullName;
-        }
-
-        private static string SimpleTypeName(Type t)
-        {
-            if (t == typeof(string)) return "string";
-            if (t == typeof(int)) return "int";
-            if (t == typeof(uint)) return "uint";
-            if (t == typeof(double)) return "double";
-            if (t == typeof(short)) return "short";
-            if (t == typeof(ushort)) return "ushort";
-            if (t == typeof(object)) return "object";
-            if (t == typeof(bool)) return "bool";
-            if (t == typeof(decimal)) return "decimal";
-
-            Type[] args = null;
-            if (t.IsGenericType)
-            {
-                var gt = t.GetGenericTypeDefinition();
-                args = t.GetGenericArguments();
-                if (gt == typeof(Nullable<>))
-                {
-                    var simple = SimpleTypeName(args[0]);
-                    if (string.IsNullOrEmpty(simple))
-                    {
-                        throw new NotSupportedException(args[0].ToString());
-                    }
-                    return simple + "?";
-                }
-
-            }
-
-            return null;
-
-            // var ns = t.Namespace;
-            // var fullName = t.FullName.Split('`')[0];
-
-            // System.Nullable`1[[System.Int16, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]
-
-            /*
-            foreach (var i in CopyMeMaker.GetNameSpaces().OrderByDescending(a => a.Length))
-            {
-                if (ns != i) continue;
-                var c = fullName.Substring(ns.Length + 1);
-                if (!c.Contains("."))
-                    fullName = c;
-                else
-                    fullName = c;
-                break;
-            }
-            if (args != null)
-            {
-                fullName += "<" + String.Join(",", args.Select(TypeName)) + ">";
-            }
-            if (fullName.Contains("+"))
-                fullName = fullName.Replace("+", ".");
-            return fullName;
-            */
-        }
-
-        #endregion
 
         public static string GetWriteMemeberName(PropertyInfo pi)
         {
             var props = pi.GetCustomAttribute<Auto.WriteMemberAttribute>();
-            return !String.IsNullOrEmpty(props?.Name) ? props.Name : pi.Name;
-        }
-
-        public const BindingFlags All =
-            BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
-
-        public static string FieldName(string x)
-        {
-            return "_" + x.Substring(0, 1).ToLower() + x.Substring(1);
+            return !string.IsNullOrEmpty(props?.Name) ? props.Name : pi.Name;
         }
 
         public static bool IsMemberStatic(MemberInfo mi)
@@ -172,5 +56,135 @@ namespace isukces.code.AutoCode
             }
             throw new NotSupportedException();
         }
+
+        public static HashSet<T> MakeCopy<T>(IEnumerable<T> source, IEnumerable<T> append = null,
+            IEnumerable<T> remove = null)
+        {
+            var s = new HashSet<T>();
+            if (source != null)
+                foreach (var i in source)
+                    s.Add(i);
+            if (append != null)
+                foreach (var i in append)
+                    s.Add(i);
+            if (remove != null)
+                foreach (var i in remove)
+                    s.Remove(i);
+            return s;
+        }
+
+        public static string TypeName(Type type, INamespaceContainer container)
+        {
+            //todo: Generic types
+            if (type == null)
+                return null;
+            if (type.IsArray)
+            {
+                var arrayRank = type.GetArrayRank();
+                var st = TypeName(type.GetElementType(), container);
+                if (arrayRank < 2)
+                    return st + "[]";
+                return string.Format("{0}[{1}]", st, new string(',', arrayRank - 1));
+            }
+            var simple = SimpleTypeName(type);
+            if (!string.IsNullOrEmpty(simple))
+                return simple;
+            if (type.DeclaringType != null)
+                return TypeName(type.DeclaringType, container) + "." + type.Name;
+
+            string fullName;
+            {
+                if (type.IsGenericType)
+                {
+                    {
+                        var nullable = GetNullableType(type);
+                        if (nullable != null)
+                            return TypeName(nullable, container) + "?";
+                    }
+                    var gt = type.GetGenericTypeDefinition();
+                    var args = type.GetGenericArguments();
+                    var args2 = string.Join(",", args.Select(a => TypeName(a, container)));
+                    fullName = gt.FullName.Split('`')[0] + "<" + args2 + ">";
+                }
+                else
+                    fullName = type.FullName;
+            }
+            var typeNamespace = type.Namespace;
+            return !string.IsNullOrEmpty(typeNamespace) &&
+                   (container?.GetNamespaces(true)?.Contains(typeNamespace) ?? false)
+                ? fullName.Substring(typeNamespace.Length + 1)
+                : fullName;
+        }
+
+        private static Type GetNullableType(Type type)
+        {
+            if ((type == null) || !type.IsGenericType)
+                return null;
+            var gt = type.GetGenericTypeDefinition();
+            var args = type.GetGenericArguments();
+            return gt == typeof(Nullable<>) ? args[0] : null;
+        }
+
+        private static string SimpleTypeName(Type t)
+        {
+            if (t == typeof(string)) return "string";
+            if (t == typeof(int)) return "int";
+            if (t == typeof(uint)) return "uint";
+            if (t == typeof(double)) return "double";
+            if (t == typeof(short)) return "short";
+            if (t == typeof(ushort)) return "ushort";
+            if (t == typeof(object)) return "object";
+            if (t == typeof(bool)) return "bool";
+            if (t == typeof(decimal)) return "decimal";
+            if (t == typeof(byte)) return "byte";
+
+            {
+                // nullable support 
+                var nullableType = GetNullableType(t);
+                if (nullableType != null)
+                {
+                    var simple = SimpleTypeName(nullableType);
+                    if (string.IsNullOrEmpty(simple))
+                        return null;
+                    return simple + "?";
+                }
+            }
+
+            return null;
+
+            // var ns = t.Namespace;
+            // var fullName = t.FullName.Split('`')[0];
+
+            // System.Nullable`1[[System.Int16, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]
+
+            /*
+            foreach (var i in CopyMeMaker.GetNameSpaces().OrderByDescending(a => a.Length))
+            {
+                if (ns != i) continue;
+                var c = fullName.Substring(ns.Length + 1);
+                if (!c.Contains("."))
+                    fullName = c;
+                else
+                    fullName = c;
+                break;
+            }
+            if (args != null)
+            {
+                fullName += "<" + String.Join(",", args.Select(TypeName)) + ">";
+            }
+            if (fullName.Contains("+"))
+                fullName = fullName.Replace("+", ".");
+            return fullName;
+            */
+        }
+
+        #endregion
+
+        #region Other
+
+        public const BindingFlags All =
+            BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+
+        #endregion
     }
 }
