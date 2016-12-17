c08293680bc1240897ce8d839078e1c1823c3eba
Piotr Stęclik <piotr@steclik.com>
2016-12-17 10:42:40 +0100

    Fixed code generation for arguments without name

--- isukces.code/Features/CsClasses/CsAttribute.cs
+++ isukces.code/Features/CsClasses/CsAttribute.cs
@@ -1,28 +1,35 @@
-﻿using System;
+﻿#region using
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using isukces.code.interfaces;
 
+#endregion
+
 namespace isukces.code
 {
-    public class CsAttribute :ClassMemberBase, ICsAttribute
+    public class CsAttribute : ClassMemberBase, ICsAttribute
     {
-
+        #region Static Methods
 
         public static implicit operator string(CsAttribute a)
         {
             return a.ToString();
         }
 
-        private static string KvToString(KeyValuePair<string, string> x)
+        private static string KeyValuePairToString(KeyValuePair<string, string> x)
         {
             return string.IsNullOrEmpty(x.Key) ? x.Value : string.Format("{0} = {1}", x.Key, x.Value);
         }
 
+        #endregion
+
+        #region Instance Methods
 
         public override string ToString()
         {
-            var values = _list.Select(KvToString);
+            var values = _list.Select(KeyValuePairToString);
             return string.Format("{0}({1})", Name, string.Join(", ", values));
             // [PrimaryKey("Id", autoIncrement = false)]
         }
@@ -48,24 +55,31 @@ namespace isukces.code
             return this;
         }
 
+        #endregion
+
+        #region Properties
 
         public string Name { get; set; }
-
-
-        private readonly List<KeyValuePair<string, string>> _list = new List<KeyValuePair<string, string>>();
 
 
         public string Code
         {
             get
             {
-                if (_list == null || _list.Count == 0)
+                if ((_list == null) || (_list.Count == 0))
                     return Name;
                 return string.Format("{0}({1})",
                     Name,
-                    string.Join(",", _list.Select(q => $"{q.Key}={q.Value}")));
+                    string.Join(",", _list.Select(KeyValuePairToString)));
             }
         }
- 
+
+        #endregion
+
+        #region Fields
+
+        private readonly List<KeyValuePair<string, string>> _list = new List<KeyValuePair<string, string>>();
+
+        #endregion
     }
 }
