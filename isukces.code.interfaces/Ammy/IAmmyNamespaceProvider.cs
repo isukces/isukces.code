d2e06225539cfd81642c1dbf99b918b592b1db65
Piotr Stęclik <piotr@steclik.pl>
2019-01-02 12:20:28 +0100

    new extension methods

--- isukces.code.interfaces/Ammy/IAmmyNamespaceProvider.cs
+++ isukces.code.interfaces/Ammy/IAmmyNamespaceProvider.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 
@@ -6,6 +7,24 @@ namespace isukces.code.interfaces.Ammy
     public interface IAmmyNamespaceProvider
     {
         [NotNull]
-        ISet<string> Namespaces { get; }   
+        ISet<string> Namespaces { get; }
+    }
+
+
+    public static class AmmyNamespaceProviderExt
+    {
+        public static void AddNamespace([NotNull] this IAmmyNamespaceProvider src, [NotNull] Type type)
+        {
+            if (src == null) throw new ArgumentNullException(nameof(src));
+            if (type == null) throw new ArgumentNullException(nameof(type));
+            var ns = type.Namespace;
+            if (!string.IsNullOrEmpty(ns))
+                src.Namespaces.Add(ns);
+        }
+
+        public static void AddNamespace<T>([NotNull] this IAmmyNamespaceProvider src)
+        {
+            src.AddNamespace(typeof(T));
+        }
     }
 }
