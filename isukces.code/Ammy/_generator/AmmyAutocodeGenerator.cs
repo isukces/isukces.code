f469029c25dbe08bad25daab540b720e18291f33
Piotr Stęclik <piotr@steclik.pl>
2019-01-13 14:50:03 +0100

    redesign events

--- isukces.code/Ammy/_generator/AmmyAutocodeGenerator.cs
+++ isukces.code/Ammy/_generator/AmmyAutocodeGenerator.cs
@@ -24,33 +24,40 @@ namespace isukces.code.Ammy
                 return;
             }
 
-            var fi   = _getFileLocation(assembly);
-            var writ = new AmmyCodeWriter();
+            var fi = _getFileLocation(assembly);
+
+            /*
             InitCodeWriter?.Invoke(this, new InitCodeWriterEventArgs
             {
-                CodeWriter = writ
+                CodeWriter = _writer
             });
+            */
 
-            var ctx = new ConversionCtx(writ);
+            var ctx = new ConversionCtx(_writer);
             {
-                var h = OnResolveSeparateLines;
+                var h = ResolveSeparateLines;
                 if (h != null)
                     ctx.OnResolveSeparateLines += (a, b) => h.Invoke(this, b);
             }
             foreach (var i in CodeParts.OrderBy(a => a.Key))
-                i.Value.WriteLineTo(writ, ctx);
+                i.Value.WriteLineTo(_writer, ctx);
 
-            var txt = writ.FullCode;
-            if (CodeFileUtils.SaveIfDifferent(txt, fi.FullName, false)) 
+            var txt = _writer.FullCode;
+            if (CodeFileUtils.SaveIfDifferent(txt, fi.FullName, false))
                 AfterSave?.Invoke(this, EventArgs.Empty);
             CodeParts = null;
+            _writer   = null;
         }
 
-        public event EventHandler AfterSave;
-        
         public void AssemblyStart(Assembly assembly, IAutoCodeGeneratorContext context)
         {
             CodeParts = new Dictionary<string, IAmmyCodePieceConvertible>();
+            _writer   = new AmmyCodeWriter();
+            StartAssembly?.Invoke(this, new StartAssemblyEventArgs
+            {
+                Assembly   = assembly,
+                CodeWriter = _writer
+            });
         }
 
         public virtual void Generate(Type type, IAutoCodeGeneratorContext context)
@@ -81,21 +88,25 @@ namespace isukces.code.Ammy
 
         protected Dictionary<string, IAmmyCodePieceConvertible> CodeParts { get; private set; }
 
+        private AmmyCodeWriter _writer;
+
         private readonly Func<Assembly, FileInfo> _getFileLocation;
 
-        public event EventHandler<ConversionCtx.ResolveSeparateLinesEventArgs> OnResolveSeparateLines;
+        public event EventHandler AfterSave;
 
-        public event EventHandler<InitCodeWriterEventArgs>    InitCodeWriter;
-        public event EventHandler<CreateMixinPrefixEventArgs> CreateMixinPrefix;
+        public event EventHandler<ConversionCtx.ResolveSeparateLinesEventArgs> ResolveSeparateLines;
+        public event EventHandler<CreateMixinPrefixEventArgs>                  CreateMixinPrefix;
+        public event EventHandler<StartAssemblyEventArgs>                      StartAssembly;
+
+        public class StartAssemblyEventArgs
+        {
+            public Assembly       Assembly   { get; set; }
+            public AmmyCodeWriter CodeWriter { get; set; }
+        }
 
         public class CreateMixinPrefixEventArgs : EventArgs
         {
             public string Prefix { get; set; }
-        }
-
-        public class InitCodeWriterEventArgs : EventArgs
-        {
-            public AmmyCodeWriter CodeWriter { get; set; }
         }
     }
 }
