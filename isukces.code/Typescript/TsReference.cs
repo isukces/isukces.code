c5622c5fa93e4765c2e9cff234a99bc2598e6b62
Piotr Stęclik <piotr@steclik.pl>
2019-03-09 17:31:03 +0100

    extends TsReference

--- isukces.code/Typescript/TsReference.cs
+++ isukces.code/Typescript/TsReference.cs
@@ -1,14 +1,69 @@
-﻿using isukces.code.interfaces;
+﻿using System;
+using System.Collections.Generic;
+using isukces.code.interfaces;
+using JetBrains.Annotations;
 
 namespace isukces.code.Typescript
 {
-    public class TsReference : ITsCodeProvider
+    public sealed class TsReference : ITsCodeProvider, IEquatable<TsReference>
     {
+        static TsReference()
+        {
+            #warning Only WINDOWS
+            PathComparer = StringComparer.OrdinalIgnoreCase;
+        }
+
+        public TsReference([NotNull] string path)
+        {
+            Path = path ?? throw new ArgumentNullException(nameof(path));
+        }
+
+        public static bool operator ==(TsReference left, TsReference right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(TsReference left, TsReference right)
+        {
+            return !Equals(left, right);
+        }
+
+        public bool Equals(TsReference other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return PathComparer.Equals(Path, other.Path);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((TsReference)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return PathComparer.GetHashCode(Path);
+        }
+
+        public override string ToString()
+        {
+            return "TsReference: " + Path;
+        }
+
         public void WriteCodeTo(ITsCodeWriter writer)
         {
             writer.WriteLine($"/// <reference path=\"{Path}\"/>");
         }
 
-        public string Path { get; set; }
+
+        public static StringComparer PathComparer { get; }
+
+        [NotNull]
+        public string Path { get; }
+
+        private static EqualityComparer<string> _pathComparer;
     }
 }
