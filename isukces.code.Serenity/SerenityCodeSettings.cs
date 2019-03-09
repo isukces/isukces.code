ae1cad42b91a72771570d0e5360b21874dfdf93a
Piotr Stęclik <piotr@steclik.pl>
2019-03-09 19:04:21 +0100

    introduce classes that helps to deal with Serenity types

---
+++ isukces.code.Serenity/SerenityCodeSettings.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace isukces.code.Serenity
+{
+    public static class SerenityCodeSettings
+    {        
+        /// <summary>
+        /// setup this with SerenityCodeSettings.SerenityRowType = typeof(Serenity.Data.Row);
+        /// </summary>
+        public static Type SerenityRowType { get; set; }
+
+        internal static Type GetSerenityRowType()
+        {
+            if (SerenityRowType == null)
+                throw new NullReferenceException(nameof(SerenityRowType) + " is null");
+            return SerenityRowType;
+        } 
+    }
+}
---
+++ isukces.code.Serenity/SerenityTypesHelper.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Reflection;
+using isukces.code.interfaces;
+
+namespace isukces.code.Serenity
+{
+    public enum SerenityClassRole
+    {
+        Unknown,
+        Row,
+        Form
+    }
+
+    public class SerenityTypesHelper
+    {
+        
+        public SerenityTypesHelper(Type clrType, SerenityClassRole role)
+        {
+            ClrType = clrType;
+            var at = new AbstractType(clrType);
+            var ns = ProcessNamespace(at.Namespace, role);
+            TsType = at.MoveToNs(ns);
+        }
+
+        public static bool IsRowType(Type rowType)
+        {
+            return SerenityCodeSettings.GetSerenityRowType()
+                .GetTypeInfo().IsAssignableFrom(rowType);
+        }
+
+        private static string ProcessNamespace(string ns, SerenityClassRole role)
+        {
+            switch (role)
+            {
+                case SerenityClassRole.Row:
+                    return TrimEnd(ns, ".Entities");
+                case SerenityClassRole.Form:
+                    return TrimEnd(ns, ".Forms");
+                case SerenityClassRole.Unknown:
+                    return ns;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(role), role, null);
+            }
+        }
+        
+        private static string TrimEnd(string text, string ending)
+        {
+            if (string.IsNullOrEmpty(ending))
+                return text;
+            if (text.EndsWith(ending, StringComparison.OrdinalIgnoreCase))
+                text = text.Substring(0, text.Length - ending.Length);
+            return text;
+        }
+
+
+        public AbstractType TsType { get; }
+
+        public Type ClrType { get; }
+    }
+}
