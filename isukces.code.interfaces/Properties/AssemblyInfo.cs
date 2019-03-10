bb6462aca2c593690091c041b6e1383e7a06fc36
Piotr Stęclik <piotr@steclik.pl>
2019-03-10 15:33:32 +0100

    combine isukces.code and isukces.code.interfaces

--- isukces.code.interfaces/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("4b6b98f5-93e9-4e5d-8719-7253a478e0f3")]
