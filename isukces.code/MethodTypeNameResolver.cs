415c7aafc5ccb7d840b4aff127b09bd834f3ae5c
Piotr Stęclik <piotr@steclik.pl>
2019-03-12 08:50:10 +0100

    another ITypeNameResolver implementation

---
+++ isukces.code/MethodTypeNameResolver.cs
@@ -0,0 +1,22 @@
+using System;
+using isukces.code.interfaces;
+using JetBrains.Annotations;
+
+namespace isukces.code
+{
+    public class MethodTypeNameResolver : ITypeNameResolver
+    {
+        public MethodTypeNameResolver([NotNull] Func<Type, string> method)
+        {
+            _method = method ?? throw new ArgumentNullException(nameof(method));
+        }
+
+        public string TypeName(Type type)
+        {
+            return _method(type);
+        }
+
+
+        private readonly Func<Type, string> _method;
+    }
+}
