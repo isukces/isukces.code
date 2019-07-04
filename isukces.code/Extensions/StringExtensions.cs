9a18b2127cbc94d1684ffcc05a052905a49c7394
Piotr Stęclik <piotr@steclik.pl>
2019-07-04 10:09:57 +0200

    Verbatim string encode

--- isukces.code/Extensions/StringExtensions.cs
+++ isukces.code/Extensions/StringExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace isukces.code
+﻿using System.Text;
+
+namespace isukces.code
 {
     public static class StringExtensions
     {
@@ -16,13 +18,41 @@
         {
             const string quote = "\"";
             const string backslash = "\\";
-
+            if (x is null)
+                return "null";
             return quote + x
                        .Replace(backslash, backslash + backslash)
                        .Replace("\r", backslash + "r")
                        .Replace("\n", backslash + "n")
                        .Replace("\t", backslash + "t")
                        .Replace(quote, backslash + quote) + quote;
+        }
+        
+        /// <summary>
+        ///     Koduje string do postaci stałej C#
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static string CsVerbatimEncode(this string x)
+        {
+            if (x is null)
+                return "null";
+
+            const string quote     = "\"";
+            const string backslash = "\\";
+            var sb = new StringBuilder();
+            sb.Append("@");
+            sb.Append(quote);
+            foreach (var c in x)
+            {
+                if (c == '\"')
+                    sb.Append(quote + quote);
+                else
+                    sb.Append(c);
+            }
+
+            sb.Append(quote);
+            return sb.ToString();
         }
 
         public static string UnCapitalize(this string x)
