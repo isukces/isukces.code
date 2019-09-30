53f605c473c1e4eaaa4be3feb66546e27c63642e
Piotr Stęclik <piotr@steclik.pl>
2019-09-30 17:27:36 +0200

    introduce AutoNavigationAttribute

---
+++ isukces.code/Db/AutoNavigationAttribute.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace isukces.code.Db
+{
+    public class AutoNavigationAttribute : Attribute
+    {
+        public AutoNavigationAttribute(string name, Type type, string inverse = null)
+        {
+            Name    = name;
+            Type    = type;
+            Inverse = inverse;
+        }
+
+        public AutoNavigationAttribute(Type type, string inverse = null)
+        {
+            Type    = type;
+            Inverse = inverse;
+        }
+
+
+        public string      Name                          { get; }
+        public Type        Type                          { get; }
+        public string      Inverse                       { get; }
+        public InverseKind GenerateInverse               { get; set; }        
+        public string      NavigationPropertyDescription { get; set; }
+    }
+
+    public enum InverseKind
+    {
+        None,
+        Single,
+        Collection
+    }
+}
