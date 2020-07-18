04754703d860ed4e26e4b26422dc13a5aaaed97e
Piotr Stęclik <piotr.steclik@isukces.com>
2020-07-18 11:32:22 +0200

    adds Xaml string encode method

--- isukces.code/Extensions/StringExtensions.cs
+++ isukces.code/Extensions/StringExtensions.cs
@@ -1,14 +1,12 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace isukces.code
 {
     public static class StringExtensions
     {
-        public static string Capitalize(this string x)
-        {
-            return x.Substring(0, 1).ToUpper() + x.Substring(1);
-        }
- 
+        public static string Capitalize(this string x) => x.Substring(0, 1).ToUpper() + x.Substring(1);
+
         /// <summary>
         ///     Koduje string do postaci stałej C#
         /// </summary>
@@ -16,18 +14,18 @@ namespace isukces.code
         /// <returns></returns>
         public static string CsEncode(this string x)
         {
-            const string quote = "\"";
+            const string quote     = "\"";
             const string backslash = "\\";
             if (x is null)
                 return "null";
             return quote + x
-                       .Replace(backslash, backslash + backslash)
-                       .Replace("\r", backslash + "r")
-                       .Replace("\n", backslash + "n")
-                       .Replace("\t", backslash + "t")
-                       .Replace(quote, backslash + quote) + quote;
+                .Replace(backslash, backslash + backslash)
+                .Replace("\r", backslash + "r")
+                .Replace("\n", backslash + "n")
+                .Replace("\t", backslash + "t")
+                .Replace(quote, backslash + quote) + quote;
         }
-        
+
         /// <summary>
         ///     Koduje string do postaci stałej C#
         /// </summary>
@@ -40,35 +38,17 @@ namespace isukces.code
 
             const string quote     = "\"";
             const string backslash = "\\";
-            var sb = new StringBuilder();
+            var          sb        = new StringBuilder();
             sb.Append("@");
             sb.Append(quote);
             foreach (var c in x)
-            {
                 if (c == '\"')
                     sb.Append(quote + quote);
                 else
                     sb.Append(c);
-            }
 
             sb.Append(quote);
             return sb.ToString();
-        }
-
-        public static string UnCapitalize(this string x)
-        {
-            return x.Substring(0, 1).ToLower() + x.Substring(1);
-        }
-        
-        public static string FirstLower(this string name) // !!!!!!
-        {
-            return name.Substring(0, 1).ToLower() + name.Substring(1);
-        }
-
-
-        public static string FirstUpper(this string name)
-        {
-            return name.Substring(0, 1).ToUpper() + name.Substring(1);
         }
 
         public static string Decamelize(this string name)
@@ -77,19 +57,47 @@ namespace isukces.code
                 return null;
             var s = new StringBuilder();
             foreach (var i in name.Trim())
-            {
                 if (s.Length == 0)
+                {
                     s.Append(i);
+                }
                 else if (char.ToUpper(i) == i)
                 {
                     s.Append(" ");
                     s.Append(char.ToLower(i));
                 }
                 else
+                {
                     s.Append(i);
-            }
+                }
+
             return s.ToString();
         }
 
+        public static string FirstLower(this string name) // !!!!!!
+            =>
+                name.Substring(0, 1).ToLower() + name.Substring(1);
+
+
+        public static string FirstUpper(this string name) => name.Substring(0, 1).ToUpper() + name.Substring(1);
+
+        public static string UnCapitalize(this string x) => x.Substring(0, 1).ToLower() + x.Substring(1);
+
+        /// <summary>
+        ///     Adds escape to { and }
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static string XamlEncode(this string x)
+        {
+            var sb = new StringBuilder(Math.Max(x.Length, 64));
+            foreach (var i in x)
+                if (i == '{')
+                    sb.Append("{}{");
+                else
+                    sb.Append(i);
+
+            return sb.ToString();
+        }
     }
 }
