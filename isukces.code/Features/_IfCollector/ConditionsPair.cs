76beb9c95abab5951ab5b6af2ea984e153ff4265
Piotr Stęclik <piotr.steclik@isukces.com>
2022-11-18 09:41:21 +0100

    adds IfCollector

---
+++ isukces.code/Features/_IfCollector/ConditionsPair.cs
@@ -0,0 +1,54 @@
+﻿namespace iSukces.Code
+{
+    public sealed class ConditionsPair
+    {
+        public ConditionsPair(string condition, string inversed = null)
+        {
+            Condition = condition;
+            if (inversed is null)
+                inversed = $"!({condition})";
+            Inversed = inversed;
+        }
+
+        public static ConditionsPair FromInversed(string inversed)
+        {
+            return new ConditionsPair($"!({inversed})", inversed);
+        }
+
+        public bool IsAlwaysTrue
+        {
+            get
+            {
+                return Condition == "true";
+            }
+        }
+        public bool IsAlwaysFalse
+        {
+            get
+            {
+                return Condition == "false";
+            }
+        }
+
+        public static ConditionsPair FromIsNull(string variable)
+        {
+            return new ConditionsPair($"{variable} is null");
+        }
+
+        public static implicit operator ConditionsPair(bool x)
+        {
+            return x
+                ? new ConditionsPair("true", "false")
+                : new ConditionsPair("false", "true");
+        }
+
+        public override string ToString()
+        {
+            return $"Condition={Condition}, Inversed={Inversed}";
+        }
+
+        public string Condition { get; }
+
+        public string Inversed { get; }
+    }
+}
