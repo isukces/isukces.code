89aa792b9d62d3255e57b6329b1bc7adfb6155b8
Piotr Stęclik <piotr.steclik@isukces.com>
2022-12-11 15:05:10 +0100

    adds GetCodeFilePathGenerator

---
+++ isukces.code/Features/_getCodeFilePathGenerator/AutocodeCustomOutputMethodAttribute.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Diagnostics;
+
+namespace iSukces.Code;
+
+[Conditional("DEBUG")]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public sealed class AutocodeCustomOutputMethodAttribute : Attribute
+{
+    public AutocodeCustomOutputMethodAttribute(string methodName) { MethodName = methodName; }
+
+    public string MethodName { get; }
+}
---
+++ isukces.code/Features/_getCodeFilePathGenerator/GetCodeFilePathGenerator.cs
@@ -0,0 +1,41 @@
+using System.Reflection;
+using iSukces.Code.AutoCode;
+using iSukces.Code.Interfaces;
+
+namespace iSukces.Code;
+
+public class GetCodeFilePathGenerator : Generators.SingleClassGenerator
+{
+    protected override void GenerateInternal()
+    {
+        const string name1  = "GetCodeFilePath";
+        var          name   = Type.GetCustomAttribute<AutocodeCustomOutputMethodAttribute>(false)?.MethodName ?? name1;
+        var          method = Type.GetMethod(name, GeneratorsHelper.AllStatic);
+        if (method is null)
+            return;
+        var m = Class.AddMethod(name, "string")
+            .WithStatic()
+            .WithBodyAsExpression("CodeFileUtils.GetCallerFilePath()");
+        m.CompilerDirective = "DEBUG";
+        m.WithAttribute(CsAttribute.Make<JetBrains.Annotations.UsedImplicitlyAttribute>(Class));
+
+        var t = Type;
+        while (t.IsNested)
+        {
+            t = t.DeclaringType;
+            if (t is null)
+                return;
+            method = t.GetMethod(name, GeneratorsHelper.AllStatic);
+            if (method != null)
+            {
+                var attribute = new CsAttribute("SuppressMessage")
+                    .WithArgumentCode("ReSharper".CsEncode())
+                    .WithArgumentCode("MemberHidesStaticFromOuterClass".CsEncode());
+                m.WithAttribute(attribute);
+                return;
+            }
+                
+        }
+            
+    }
+}
