83fc01844a623024977f3fac359c45904aca7c33
Piotr Stęclik <piotr.steclik@isukces.com>
2023-08-19 12:52:31 +0200

    fixes demo code

--- samples/Sample.AppWithCSharpCodeEmbedding/AutoCode/CodeFilePathContextProvider.cs
+++ samples/Sample.AppWithCSharpCodeEmbedding/AutoCode/CodeFilePathContextProvider.cs
@@ -1,18 +1,17 @@
 using System;
 using iSukces.Code.AutoCode;
 
-namespace Sample.AppWithCSharpCodeEmbedding.AutoCode
+namespace Sample.AppWithCSharpCodeEmbedding.AutoCode;
+
+internal class CodeFilePathContextProvider : ICsOutputProvider
 {
-    internal class CodeFilePathContextProvider : ICsOutputProvider
+    public CsOutputFileInfo GetOutputFileInfo(Type type)
     {
-        public CsOutputFileInfo GeOutputFileInfo(Type type)
-        {
-            var method = type.GetMethod("GetCodeFilePath", GeneratorsHelper.AllStatic);
-            if (method is null)
-                return null;
-            if (method.Invoke(null, null) is string fileName)
-                return new CsOutputFileInfo(fileName, true);
+        var method = type.GetMethod("GetCodeFilePath", GeneratorsHelper.AllStatic);
+        if (method is null)
             return null;
-        }
+        if (method.Invoke(null, null) is string fileName)
+            return new CsOutputFileInfo(fileName, true);
+        return null;
     }
 }
