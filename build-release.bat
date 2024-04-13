4072a3bc4830ab82a8c2e52be0f286586338011b
Piotr Stęclik <piotr.steclik@isukces.com>
2024-04-13 18:01:44 +0200

    Adds replace method

---
+++ build-release.bat
@@ -0,0 +1,2 @@
+dotnet build isukces.code.nuget.sln -c RELEASE
+rem xcopy C:\programs\isukces\dotnetLib\isukces.code\isukces.code\bin\Debug C:\Users\piotr\.nuget\packages\isukces.code\1.23.1218.202\lib /s /y
--- isukces.code/CodeWriter.cs
+++ isukces.code/CodeWriter.cs
@@ -3,156 +3,122 @@ using System.Linq;
 using System.Text;
 using iSukces.Code.Interfaces;
 
-namespace iSukces.Code
+namespace iSukces.Code;
+
+public abstract class CodeWriter : ICodeWriter
 {
-    public abstract class CodeWriter : ICodeWriter
+    protected CodeWriter(ILangInfo langInfo)
     {
-        protected CodeWriter(ILangInfo langInfo)
+        LangInfo = langInfo;
+    }
+
+    public void Append(string text) => _sb.Append(text);
+
+    protected virtual string GetCodeForSave() => Code;
+
+    public void Save(string filename)
+    {
+        var fi = new FileInfo(filename);
+        fi.Directory?.Create();
+        using var fs = new FileStream(filename, File.Exists(filename) ? FileMode.Create : FileMode.CreateNew);
+        SaveToStream(fs);
+    }
+
+    public bool SaveIfDifferent(string filename)
+    {
+        if (!File.Exists(filename))
         {
-            LangInfo = langInfo;
-        }
-
-        public void Append(string text)
-        {
-            _sb.Append(text);
-        }
-/*
-        public void Block(string open, string customCloseText, Action method)
-        {
-            Writeln(open);
-            this.IncIndent();
-            method();
-            this.Close(customCloseText);
-        }
-
-        public void Block(string open, Action method)
-        {
-            Writeln(open);
-            this.IncIndent();
-            method();
-            this.Close();
-        }
-
-
-        public void Clear()
-        {
-            _sb     = new StringBuilder();
-            Indent  = 0;
-            // opening = new Dictionary<int, string>();
-        }
-*/
-
-        protected virtual string GetCodeForSave()
-        {
-            return Code;
-        }
-
-        public void Save(string filename)
-        {
-            var fi = new FileInfo(filename);
-            fi.Directory.Create();
-            using(var fs = new FileStream(filename, File.Exists(filename) ? FileMode.Create : FileMode.CreateNew))
-            {
-                SaveToStream(fs);
-            }
-        }
-
-        public bool SaveIfDifferent(string filename)
-        {
-            if (!File.Exists(filename))
-            {
-                Save(filename);
-                return true;
-            }
-
-            byte[] newa;
-            var existing = File.ReadAllBytes(filename);
-            using(var fs = new MemoryStream())
-            {
-                SaveToStream(fs);
-                newa = fs.ToArray();
-            }
-
-            if (newa.SequenceEqual(existing)) 
-                return false;
-            File.WriteAllBytes(filename, newa);
+            Save(filename);
             return true;
         }
 
-        private void SaveToStream(Stream stream)
+        byte[] newa;
+        var existing = File.ReadAllBytes(filename);
+        using(var fs = new MemoryStream())
         {
-            if (LangInfo.AddBOM)
-                stream.Write(new byte[] {0xEF, 0xBB, 0xBF}, 0, 3);
-            var x = Encoding.UTF8.GetBytes(GetCodeForSave());
-            stream.Write(x, 0, x.Length);
+            SaveToStream(fs);
+            newa = fs.ToArray();
         }
 
-
-        // public bool AutoCloseComment { get; set; }
-
-        public override string ToString()
-        {
-            return Code;
-        }
-
-       
-
-        /// <summary>
-        ///     opis języka
-        /// </summary>
-        public ILangInfo LangInfo { get; }
-
-        public string[] Lines
-        {
-            get
-            {
-                var tmp = Code;
-                var l   = tmp.Split('\n');
-                for (var i = 0; i < l.Length; i++)
-                    l[i] = l[i].Replace("\r", "");
-                return l;
-            }
-        }
-
-        /// <summary>
-        ///     Czy trimować tekst
-        /// </summary>
-        public bool TrimText { get; set; }
-
-        public string Code
-        {
-            get
-            {
-                var x = _sb.ToString();
-                if (TrimText) return x.Trim();
-                return x;
-            }
-        }
-
-
-        public int Indent
-        {
-            get => _indent;
-            set
-            {
-                if (value < 0)
-                    value = 0;
-                if (value == _indent)
-                    return;
-
-                _indent = value;
-                _indentStr = value > 0
-                    ? new string(' ', IndentSize * value)
-                    : string.Empty;
-            }
-        }
-
-
-        private string _indentStr = "";
-
-        // private Dictionary<int, string> opening = new Dictionary<int, string>();
-        private readonly StringBuilder _sb = new StringBuilder();
-        private int _indent;
-        private const int IndentSize = 4;
+        if (newa.SequenceEqual(existing)) 
+            return false;
+        File.WriteAllBytes(filename, newa);
+        return true;
     }
+
+    private void SaveToStream(Stream stream)
+    {
+        if (LangInfo.AddBOM)
+            stream.Write(new byte[] {0xEF, 0xBB, 0xBF}, 0, 3);
+        var x = Encoding.UTF8.GetBytes(GetCodeForSave());
+        stream.Write(x, 0, x.Length);
+    }
+    
+    public override string ToString() => Code;
+
+    /// <summary>
+    ///     opis języka
+    /// </summary>
+    public ILangInfo LangInfo { get; }
+
+    public string[] Lines
+    {
+        get
+        {
+            var tmp = Code;
+            var l   = tmp.Split('\n');
+            for (var i = 0; i < l.Length; i++)
+                l[i] = l[i].Replace("\r", "");
+            return l;
+        }
+    }
+
+    /// <summary>
+    ///     Czy trimować tekst
+    /// </summary>
+    public bool TrimText { get; set; }
+
+    public void Replace(string search, string replaceBy)
+    {
+        var code = _sb.ToString();
+        code = code.Replace(search, replaceBy);
+        _sb.Clear();
+        _sb.Append(code);
+    }
+
+    public string Code
+    {
+        get
+        {
+            var x = _sb.ToString();
+            if (TrimText) 
+                return x.Trim();
+            return x;
+        }
+    }
+        
+    public int Indent
+    {
+        get => _indent;
+        set
+        {
+            if (value < 0)
+                value = 0;
+            if (value == _indent)
+                return;
+
+            _indent = value;
+            _indentStr = value > 0
+                ? new string(' ', IndentSize * value)
+                : string.Empty;
+        }
+    }
+
+
+    private string _indentStr = "";
+
+    // private Dictionary<int, string> opening = new Dictionary<int, string>();
+    private readonly StringBuilder _sb = new StringBuilder();
+    private int _indent;
+    private const int IndentSize = 4;
 }
