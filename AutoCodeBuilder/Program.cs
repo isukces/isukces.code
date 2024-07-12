b3e5f000d7abfd83614ab950b36a93975cef8a45
Piotr Stęclik <piotr@steclik.com>
2024-07-12 12:57:31 +0200

    public nuget release 1.24.712.208

--- AutoCodeBuilder/Program.cs
+++ AutoCodeBuilder/Program.cs
@@ -12,7 +12,7 @@ namespace AutoCodeBuilder
         }
         private static void Make2()
         {
-#if DEBUG
+#if DEBUGx
             
             var myAssembly = typeof(Program).Assembly;
             var solutionDir = CodeUtils.SearchFoldersUntilFileExists(myAssembly, "iSukces.Code.sln");
@@ -27,7 +27,6 @@ namespace AutoCodeBuilder
             autoCodeGenerator.FileNamespaces.Add("Irony.Interpreter.Ast");
             autoCodeGenerator.FileNamespaces.Add("Irony.Parsing");
             autoCodeGenerator.CodeGenerators.Add(new IronParserGenerator(AmmyGrammarAutogeneratorInfo.Items(), typeof(AmmyGrammar)));
-
             var scanAssembly = typeof(AmmyGrammarAutogeneratorInfo).Assembly;
             autoCodeGenerator.Make(scanAssembly);
 
