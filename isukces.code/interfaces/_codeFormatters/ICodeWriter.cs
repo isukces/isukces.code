406ffa8373aeafe27bdeeae5ca7bc717d0c5e6cf
Piotr Stęclik <piotr@steclik.com>
2024-11-04 09:08:42 +0100

    improves ICodeWriter extension methods

--- isukces.code/interfaces/_codeFormatters/ICodeWriter.cs
+++ isukces.code/interfaces/_codeFormatters/ICodeWriter.cs
@@ -5,169 +5,176 @@ using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 
-namespace iSukces.Code.Interfaces
+namespace iSukces.Code.Interfaces;
+
+public interface ICodeWriter
 {
-    public interface ICodeWriter
+    void Append(string text);
+
+    string Code { get; }
+
+    ILangInfo LangInfo { get; }
+
+    int Indent { get; set; }
+}
+
+public static class CodeFormatterExt
+{
+    public static T Close<T>(this T writer, string customCloseText)
+        where T : ICodeWriter
     {
-        void Append(string text);
-
-        string Code { get; }
-
-        ILangInfo LangInfo { get; }
-
-        int Indent { get; set; }
+        writer.DecIndent();
+        writer.WriteLine(customCloseText);
+        return writer;
     }
 
-    public static class CodeFormatterExt
+    public static T Close<T>(this T writer, bool addEmptyLine = false)
+        where T : ICodeWriter
     {
-        public static void Close(this ICodeWriter src, string customCloseText)
-        {
-            src.DecIndent();
-            src.WriteLine(customCloseText);
-        }
-
-        public static void Close(this ICodeWriter src, bool addEmptyLine = false)
-        {
-            var langInfo = src.LangInfo;
-            src.DecIndent();
+        var langInfo = writer.LangInfo;
+        writer.DecIndent();
 #if AutoCloseText
             if (AutoCloseComment)
                 src.WriteLine(langInfo.CloseText + " " + langInfo.OneLineComment + " " + opening[Indent]);
             else
 #endif
-            src.WriteLine(langInfo.CloseText);
-            if (addEmptyLine)
-                src.WriteLine();
-        }
+        writer.WriteLine(langInfo.CloseText);
+        if (addEmptyLine)
+            writer.WriteLine();
+        return writer;
+    }
 
-        public static ICodeWriter CloseNl(this ICodeWriter src)
-        {
-            src.Close(false);
-            src.WriteLine(string.Empty);
-            return src;
-        }
+    public static T CloseNl<T>(this T writer)
+        where T : ICodeWriter
+    {
+        writer.Close();
+        writer.WriteLine(string.Empty);
+        return writer;
+    }
 
-        public static void DoWithKeepingIndent(this ICodeWriter obj, Action action)
-        {
-            var indentBefore = obj.Indent;
-            action();
-            if (obj.Indent == indentBefore) return;
-            // some warning should be created here
-            obj.Indent = indentBefore;
-        }
+    public static T DecIndent<T>(this T writer)
+        where T : ICodeWriter
+    {
+        writer.Indent--;
+        return writer;
+    }
 
-        public static string GetCodeTrim(this ICodeWriter writer)
-        {
-            if (writer == null) throw new ArgumentNullException(nameof(writer));
-            return writer.Code.Trim();
-        }
+    public static T DoWithKeepingIndent<T>(this T writer, Action action)
+        where T : ICodeWriter
+    {
+        var indentBefore = writer.Indent;
+        action();
+        if (writer.Indent == indentBefore) return writer;
+        // some warning should be created here
+        writer.Indent = indentBefore;
+        return writer;
+    }
+
+    public static string GetCodeTrim(this ICodeWriter writer)
+    {
+        if (writer == null) throw new ArgumentNullException(nameof(writer));
+        return writer.Code.Trim();
+    }
+
+    public static T IncIndent<T>(this T writer)
+        where T : ICodeWriter
+    {
+        writer.Indent++;
+        return writer;
+    }
 
 
-        public static T Open<T>(this T src, string x) where T:ICodeWriter
-        {
+    public static T Open<T>(this T writer, string x)
+        where T : ICodeWriter
+    {
 #if AutoCloseText
             opening[src.Indent] = x;
 #endif
-            src.WriteLine(x);
-            src.WriteLine(src.LangInfo.OpenText);
-            src.Indent++;
-            return src;
-        }
+        writer.WriteLine(x);
+        writer.WriteLine(writer.LangInfo.OpenText);
+        writer.Indent++;
+        return writer;
+    }
 
-        public static T Open<T>(this T src, string format, params object[] args) where T : ICodeWriter
-        {
-            src.Open(string.Format(format, args));
-            return src;
-        }
+    public static T Open<T>(this T writer, string format, params object[] args)
+        where T : ICodeWriter
+    {
+        writer.Open(string.Format(format, args));
+        return writer;
+    }
 
+    public static T OpenBrackets<T>(this T writer)
+        where T : ICodeWriter
+    {
+        return writer.WriteLine(writer.LangInfo.OpenText).IncIndent();
+    }
 
-        public static void OpenBrackets(this ICodeWriter src)
-        {
-            src.WriteLine(src.LangInfo.OpenText);
-            src.IncIndent();
-        }
+    public static T SplitWriteLine<T>(this T writer, string? text)
+        where T : ICodeWriter
+    {
+        if (string.IsNullOrEmpty(text))
+            return writer;
+        var query = from i in text!.Split('\r', '\n')
+            where !string.IsNullOrEmpty(i)
+            select i.TrimEnd();
+        foreach (var i in query)
+            writer.WriteLine(i);
+        return writer;
+    }
 
-        public static T DecIndent<T>(this T obj)
-            where T : ICodeWriter
-        {
-            obj.Indent--;
-            return obj;
-        }
+    // [Obsolete("Use CsType instead of string", GlobalSettings.WarnObsolete)]
+    public static T WithOpen<T>(this T writer, string text)
+        where T : ICodeWriter
+    {
+        writer.Open(text);
+        return writer;
+    }
 
-        public static T IncIndent<T>(this T obj)
-            where T : ICodeWriter
-        {
-            obj.Indent++;
-            return obj;
-        }
+    /*
+    public static T WithClose<T>(this T writer)
+        where T : ICodeWriter
+    {
+        writer.Close();
+        return writer;
+    }*/
 
-        // [Obsolete("Use CsType instead of string", GlobalSettings.WarnObsolete)]
-        public static T WithOpen<T>(this T obj, string text)
-            where T : ICodeWriter
-        {
-            obj.Open(text);
-            return obj;
-        }
+    public static T WriteLine<T>(this T writer)
+        where T : ICodeWriter
+    {
+        writer.Append("\r\n");
+        return writer;
+    }
 
-        public static T WithClose<T>(this T obj)
-            where T : ICodeWriter
-        {
-            obj.Close();
-            return obj;
-        }
+    public static T WriteLine<T>(this T writer, string text)
+        where T : ICodeWriter
+    {
+        writer.WriteIndent().Append(text + "\r\n");
+        return writer;
+    }
+    
+    [StringFormatMethod("format")]
+    public static T WriteLine<T>(this T writer, string format, params object[] args)
+        where T : ICodeWriter
+    {
+        var text = string.Format(format, args);
+        return writer.WriteLine(text);
+    }
 
-        public static T WriteLine<T>(this T obj)
-            where T : ICodeWriter
-        {
-            obj.Append("\r\n");
-            return obj;
-        }
+    public static T WritelineNoIndent<T>(this T writer, string compilerCode)
+        where T : ICodeWriter
+    {
+        var indentBefore = writer.Indent;
+        writer.Indent = 0;
+        writer.WriteLine(compilerCode);
+        writer.Indent = indentBefore;
+        return writer;
+    }
 
-        public static T WriteLine<T>(this T obj, string text)
-            where T : ICodeWriter
-        {
-            obj.WriteIndent().Append(text + "\r\n");
-            return obj;
-        }
-        
-        public static T WriteLines<T>(this T obj, IEnumerable<string> texts)
-            where T : ICodeWriter
-        {
-            foreach (var line in texts)
-                WriteLine(obj, line);
-            return obj;
-        }
-
-        public static T SplitWriteLine<T>(this T x, string? text)
-            where T : ICodeWriter
-        {
-            if (string.IsNullOrEmpty(text))
-                return x;
-            var query = from i in text.Split('\r', '\n')
-                where !string.IsNullOrEmpty(i)
-                select i.TrimEnd();
-            foreach (var i in query)
-                x.WriteLine(i);
-            return x;
-        }
-
-
-        [StringFormatMethod("format")]
-        public static T WriteLine<T>(this T src, string format, params object[] args)
-            where T : ICodeWriter
-        {
-            var text = string.Format(format, args);
-            return src.WriteLine(text);
-        }
-
-        public static T WritelineNoIndent<T>(this T obj, string compilerCode)
-            where T : ICodeWriter
-        {
-            var indentBefore = obj.Indent;
-            obj.Indent = 0;
-            obj.WriteLine(compilerCode);
-            obj.Indent = indentBefore;
-            return obj;
-        }
+    public static T WriteLines<T>(this T writer, IEnumerable<string> texts)
+        where T : ICodeWriter
+    {
+        foreach (var line in texts)
+            WriteLine(writer, line);
+        return writer;
     }
 }
